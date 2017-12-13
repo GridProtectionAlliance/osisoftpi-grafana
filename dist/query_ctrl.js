29b395287deb2a1905543119426a0431658c12a7
Khalil AlQassab <khalil_alqassab@tatweerpetroleum.com>
2017-12-13 18:06:15 +0300

    Update query_ctrl.js
    In file osisoftpi-grafana-master\dist\query_ctrl.js
    This statement
    _this.target.target = _this.target.target || 'select element';
    Was replaced with
    _this.target.target = _this.target.target || ‘;';

--- dist/query_ctrl.js
+++ dist/query_ctrl.js
@@ -117,7 +117,7 @@ System.register(['angular', 'lodash', 'app/plugins/sdk', './css/query-editor.css
           _this.target.summary.nodata = _this.target.summary.nodata || 'Null';
           _this.target.summary.interval = _this.target.summary.interval || '5m';
 
-          _this.target.target = _this.target.target || 'select element';
+          _this.target.target = _this.target.target || ';';
 
           _this.target.interpolate = _this.target.interpolate || { enable: false };
           if (_this.target.interpolate === false || _this.target.interpolate === true) {
